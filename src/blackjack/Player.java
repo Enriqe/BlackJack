131672378fc1f94e5618ff177d1f8fe4e28f198e
Alejandro Sanchez <alejandro.s1993@gmail.com>
2014-11-06 23:00:56 -0600

    changes to player

--- src/blackjack/Player.java
+++ src/blackjack/Player.java
@@ -1,13 +1,28 @@
 package blackjack;
 
+
 public class Player {
 
 	public boolean turn;
 	public String name;
-	public Object hand;
-	public void addCard() {
-		// TODO Auto-generated method stub
-		
+	public Hand hand;
+	
+	public Player(String name){
+		this.name = name;
+		this.hand = makeHand();
+	}
+	
+	public Hand makeHand(){
+		return new Hand();
 	}
 
+	public boolean isTurn() {
+		return turn;
+	}
+
+	public void setTurn(boolean turn) {
+		this.turn = turn;
+	}
+	
+	
 }
