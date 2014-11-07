2b52c5d49c3d442c5f6158fe4d7a4090a115114c
MarcoRmz <marcormz19@gmail.com>
2014-11-07 00:48:44 -0600

    adding menu

--- src/blackjack/Game.java
+++ src/blackjack/Game.java
@@ -8,6 +8,13 @@ public class Game {
     public int recorrido = 0;
     public Card cartaActual;
     
+    public Game(){
+    	deck = new Deck();
+    	player = new Player();
+    	dealer = new Player();
+    	recorrido = 0;
+    }
+    
     public Game(Deck d, Player p, Player deal, int r){
     	deck = d;
     	player = p;
@@ -49,10 +56,26 @@ public class Game {
     
     public static void main(String [] args){
     	Scanner in = new Scanner(System.in);
+    	Game Juego = new Game();
     	System.out.println("BlackJack!");
+    	Juego.deal();
     	System.out.println("Score: " + player.getScore());
-    	System.out.println("Hit?");
+    	System.out.println("Hit? (1)");
+    	System.out.println("Stay? (2)");
+    	System.out.println("Quit? (3)");
     	int i = in.nextInt();
+    	
+    	if(i == 1){
+    		if(player.turn){
+    			.hit(true);
+    		}
+    	}
+    	else if(i == 2){
+    		//stay
+    	}
+    	else if(i == 3){
+    		//quit
+    	}
     	String s = in.next();
     	System.out.println(i);
     	System.out.println(s);
