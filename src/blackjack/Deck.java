e5b5eeacec0d3a3319d5f08840ede5a6d775f306
MarcoRmz <marcormz19@gmail.com>
2014-11-10 22:11:19 -0600

    constructor fix

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -5,46 +5,46 @@ public class Deck {
 	
 	public Card[] deckCards = new Card[52];
 	
-	public void Deck(){
+	public Deck(){
 		int count = 0;
 		for(int i = 0; i < 13; i++){
-			deckCards[i].setSuit("Heart");
+			this.deckCards[i].setSuit("Heart");
 			count++;
-			deckCards[i].setRank(count);
+			this.deckCards[i].setRank(count);
 		}
 		
 		count = 0;
 		for(int i = 13; i < 26; i++){
-			deckCards[i].setSuit("Diamond");
+			this.deckCards[i].setSuit("Diamond");
 			count++;
-			deckCards[i].setRank(count);
+			this.deckCards[i].setRank(count);
 		}
 		
 		count = 0;
 		for(int i = 26; i < 39; i++){
-			deckCards[i].setSuit("Spade");
+			this.deckCards[i].setSuit("Spade");
 			count++;
-			deckCards[i].setRank(count);
+			this.deckCards[i].setRank(count);
 		}
 		
 		count = 0;
 		for(int i = 39; i < 52; i++){
-			deckCards[i].setSuit("Club");
+			this.deckCards[i].setSuit("Club");
 			count++;
-			deckCards[i].setRank(count);
+			this.deckCards[i].setRank(count);
 		}
 		int index;
 		Card temp;
 		Random random = new Random();
-		for(int i = deckCards.length - 1; i > 0; i--) {
+		for(int i = this.deckCards.length - 1; i > 0; i--) {
 			index = random.nextInt(i + 1);
-			temp = deckCards[index];
-			deckCards[index] = deckCards[i];
-			deckCards[i] = temp;
+			temp = this.deckCards[index];
+			this.deckCards[index] = this.deckCards[i];
+			this.deckCards[i] = temp;
 		}
 		System.out.println("Deck:");
 		for(int i = 0; i < 52; i++) {
-			System.out.println(deckCards[i].getSuit() + deckCards[i].getRank());
+			System.out.println(this.deckCards[i].getSuit() + this.deckCards[i].getRank());
 		}
 	}
 	
