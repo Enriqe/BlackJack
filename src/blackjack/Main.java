189e4c9474dead2d347e0b1e7f85e1103fc868f7
MarcoRmz <marcormz19@gmail.com>
2014-11-17 16:42:59 -0600

    validate under 21 in first draw & added new game

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -17,51 +17,66 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        
-        Game Juego = new Game(player, dealer, recorrido);
-        System.out.println("BlackJack!");
-        Juego.deal();
-        
-        System.out.println("What is your name?");
-        String s = in.next();
-        player.setName(s);
-        
-        System.out.println(player.getName() + " it's your turn.");
-        
-        //System.out.println("Your hand: ");
-        //for(int i = 0; i < player.hand.cards.length; i++){
-        	//System.out.println(player.hand.cards[i].getSuit() + "  "+ player.hand.cards[i].getRank());
-        //}
-        
         int i; // user input for the game
-        do {
-        	System.out.println("Your hand: ");
-            player.hand.show();
-            System.out.println("\nYour score: " + player.getScore());
-            menu();
-            i = in.nextInt();
-            
-            if(i == 1){
-    			if(player.turn){
-    				Juego.hit();
-    				player.turn = !player.turn;
-    				dealer.turn = !dealer.turn;
-    			}else{
-    				Juego.hit();
-    				player.turn = !player.turn;
-    				dealer.turn = !dealer.turn;
-    			}
-    		}
-            else if(i == 2){
-                Juego.stay();
-            }
-            else if(i == 3){
-                System.out.println("Ended Game");
-                System.exit(0);
-            }
-        } while((player.turn || player.getScore() <= 21) && i != 2 && i != 3);
         
-        Juego.stay();
+        do {
+        	Game Juego = new Game(player, dealer, recorrido);
+            System.out.println("BlackJack!");
+            Juego.deal();
+            
+            System.out.println("What is your name?");
+            String s = in.next();
+            player.setName(s);
+            
+            System.out.println(player.getName() + " it's your turn.");
+            
+            //System.out.println("Your hand: ");
+            //for(int i = 0; i < player.hand.cards.length; i++){
+            	//System.out.println(player.hand.cards[i].getSuit() + "  "+ player.hand.cards[i].getRank());
+            //}
+            
+            do {
+            	System.out.println("Your hand: ");
+                player.hand.show();
+                System.out.println("\nYour score: " + player.getScore());
+                if(player.getScore() > 21){
+                	System.out.println("You Lose");
+                	System.out.println("New Game? (4)");
+                	System.out.println("Quit? (3)");
+                	i = in.nextInt();
+                }
+                else {
+                	menu();
+                    i = in.nextInt();
+                    
+                    if(i == 1){
+            			if(player.turn){
+            				Juego.hit();
+            				player.turn = !player.turn;
+            				dealer.turn = !dealer.turn;
+            			}else{
+            				Juego.hit();
+            				player.turn = !player.turn;
+            				dealer.turn = !dealer.turn;
+            			}
+            		}
+                    else if(i == 2){
+                        Juego.stay();
+                        System.out.println("New Game? (4)");
+                    	System.out.println("Quit? (3)");
+                    	i = in.nextInt();
+                    }
+                    else if(i == 3){
+                        System.out.println("Ended Game");
+                        System.exit(0);
+                    }
+                    else if(i == 4){
+                    	System.out.println("Starting New Game");
+                    }
+                }
+            } while((player.turn || player.getScore() <= 21) && i != 2 && i != 3 && i !=4);
+        }while(i != 3);
+        
         
          in.close();
     }
