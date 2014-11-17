08c12f8cb57702a7cf15a35bb150e3bb431c2853
MarcoRmz <marcormz19@gmail.com>
2014-11-17 16:55:48 -0600

    fixing reset to new game

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -3,8 +3,8 @@ package blackjack;
 import java.util.*;
 
 public class Main {
-	public Deck deck;
-    public static Player player = new Player(), dealer = new Player();
+	public static Deck deck;
+    public static Player player, dealer;
     public static int recorrido = 0;
     
     private static void menu(){
@@ -12,6 +12,7 @@ public class Main {
     	System.out.println("Hit? (1)");
         System.out.println("Stay? (2)");
         System.out.println("Quit? (3)");
+        System.out.println("New Game? (4)");
     }
 	
     public static void main(String[] args) {
@@ -20,6 +21,10 @@ public class Main {
         int i; // user input for the game
         
         do {
+        	recorrido = 0;
+        	player = new Player();
+        	dealer = new Player();
+        	deck = new Deck();
         	Game Juego = new Game(player, dealer, recorrido);
             System.out.println("BlackJack!");
             Juego.deal();
