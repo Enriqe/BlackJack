4ba3dae038b98484c5e6efc76c1e31acfcccd9b9
Alejandro Sanchez <alejandro.s1993@gmail.com>
2014-11-17 18:45:20 -0600

    dealer now looses when he has over 21

--- src/blackjack/Game.java
+++ src/blackjack/Game.java
@@ -2,50 +2,64 @@ package blackjack;
 
 
 public class Game {
-    public static Player player = new Player(), dealer = new Player();
-    public int recorrido = 0;
-    public static Deck deck = new Deck();
-    
-    public Game(){
-    	recorrido = 0;
-    }
-    
-    public Game(Player p, Player deal, int r){
-    	player = p;
-    	dealer = deal;
-    	recorrido = r;
-    }
+	public static Player player = new Player(), dealer = new Player();
+	public int recorrido = 0;
+	public static Deck deck = new Deck();
 
-    public void hit(){
-    	if(player.turn){
-            player.hand.addCard(deck.deckCards[recorrido]);
-            recorrido++;
-            
-    	}
-    	else{
-            dealer.hand.addCard(deck.deckCards[recorrido]);
-            recorrido++;
-            
-    	}
-    }
-    
-    public void stay() {
-    	player.turn = !player.turn;
-        dealer.turn = !dealer.turn;
-    	while(dealer.getScore() < player.getScore() && dealer.getScore() <= 21){
-            this.hit();
-        }
-    	if(player.getScore() > dealer.getScore() && (player.getScore() <=21)){
-    		System.out.println("Your Score: " + player.getScore());
-    		System.out.println("Dealer Score: " + dealer.getScore());
-    		System.out.println("You Win!");
-    	}
-    	else {
-    		System.out.println("Your Score: " + player.getScore());
-    		System.out.println("Dealer Score: " + dealer.getScore());
-    		System.out.println("You Lose!");
-    	}
-    	/*
+	public Game(){
+		recorrido = 0;
+	}
+
+	public Game(Player p, Player deal, int r){
+		player = p;
+		dealer = deal;
+		recorrido = r;
+	}
+
+	public void hit(){
+		if(player.turn){
+			player.hand.addCard(deck.deckCards[recorrido]);
+			recorrido++;
+
+		}
+		else{
+			dealer.hand.addCard(deck.deckCards[recorrido]);
+			recorrido++;
+
+		}
+	}
+
+	public void stay() {
+		player.turn = !player.turn;
+		dealer.turn = !dealer.turn;
+		while(dealer.getScore() < player.getScore() && dealer.getScore() <= 21){
+			this.hit();
+		}
+		if(player.getScore() == dealer.getScore()){
+			System.out.println("Your Score: " + player.getScore());
+			System.out.println("Dealer Score: " + dealer.getScore());
+			System.out.println("You Lose!");
+		}
+
+		if(player.getScore() > dealer.getScore() && (player.getScore() <=21)){
+			System.out.println("Your Score: " + player.getScore());
+			System.out.println("Dealer Score: " + dealer.getScore());
+			System.out.println("You Win!");
+		}
+		else {
+			if(dealer.getScore() > 21){
+				System.out.println("Your Score: " + player.getScore());
+				System.out.println("Dealer Score: " + dealer.getScore());
+				System.out.println("You Win!");
+			}
+			else{
+				System.out.println("Your Score: " + player.getScore());
+				System.out.println("Dealer Score: " + dealer.getScore());
+				System.out.println("You Lose!");
+
+			}
+		}
+		/*
     	if(player.turn){
             player.turn = !player.turn;
             dealer.turn = !dealer.turn;
@@ -67,32 +81,32 @@ public class Game {
         		System.out.println("You Lose!");
         	}
         }
-        */
-    }
-    
-    public void deal() {
-        recorrido = 0;
-        
-        player.turn = true;
-        player.name = "";
-        dealer.name = "Dealer";
-        dealer.turn = false;
-        
-        player.hand.addCard(deck.getCard(recorrido));
-        //System.out.println("Carta: " + deck.getCard(recorrido).getSuit() + deck.getCard(recorrido).getRank());
-        recorrido++;
-        
-        player.hand.addCard(deck.getCard(recorrido));
-        //System.out.println("Carta player: " + deck.deckCards[recorrido].getSuit() + deck.deckCards[recorrido].getRank());
-        recorrido++;
-        
-        dealer.hand.addCard(deck.getCard(recorrido));
-        //System.out.println("Carta dealer: " + deck.deckCards[recorrido].getSuit() + deck.deckCards[recorrido].getRank());
-        recorrido++;     
-        
-        dealer.hand.addCard(deck.getCard(recorrido));
-        //System.out.println("Carta dealer: " + deck.deckCards[recorrido].getSuit() + deck.deckCards[recorrido].getRank());
-        recorrido++;
-    };
- 
+		 */
+	}
+
+	public void deal() {
+		recorrido = 0;
+
+		player.turn = true;
+		player.name = "";
+		dealer.name = "Dealer";
+		dealer.turn = false;
+
+		player.hand.addCard(deck.getCard(recorrido));
+		//System.out.println("Carta: " + deck.getCard(recorrido).getSuit() + deck.getCard(recorrido).getRank());
+		recorrido++;
+
+		player.hand.addCard(deck.getCard(recorrido));
+		//System.out.println("Carta player: " + deck.deckCards[recorrido].getSuit() + deck.deckCards[recorrido].getRank());
+		recorrido++;
+
+		dealer.hand.addCard(deck.getCard(recorrido));
+		//System.out.println("Carta dealer: " + deck.deckCards[recorrido].getSuit() + deck.deckCards[recorrido].getRank());
+		recorrido++;     
+
+		dealer.hand.addCard(deck.getCard(recorrido));
+		//System.out.println("Carta dealer: " + deck.deckCards[recorrido].getSuit() + deck.deckCards[recorrido].getRank());
+		recorrido++;
+	};
+
 }
